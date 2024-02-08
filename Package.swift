9c971940b8343f4b7ef508bffe57863f60085a82
Aleksey Zamulla <aleksey.zamulla@jetbrains.com>
2024-02-08 17:26:42 +0400

    ExampleDependency fix?

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
      .iOS(.v14),
    ],
    products: [
-      .library(name: "shared", targets: ["shared"])
+      .library(name: "ExampleDependency", targets: ["ExampleDependency"])
    ],
    targets: [
             .binaryTarget(name: "shared", url: "https://github.com/zamulla/spmtest/releases/download/5/shared.xcframework.zip",checksum:"6ad6e121869bf07e3d4fb6ccc82320c224fc971f123cc53baad997fccd70572f")
