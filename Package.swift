3567c8a6821c6ade1af1770a2f78e3f03756e36e
Aleksey Zamulla <aleksey.zamulla@jetbrains.com>
2024-02-08 13:08:27 +0400

    comma

--- Package.swift
+++ Package.swift
@@ -11,5 +11,5 @@ let package = Package(
    ],
    targets: [
             .binaryTarget(name: "shared", url: "https://github.com/zamulla/spmtest/releases/download/2/shared.xcframework.zip",checksum:"575af8a01f09684e8cc35d35a33823cf117f78da31d5bf792cbcae6b8fe861f1")
-   ],
+   ]
 )
