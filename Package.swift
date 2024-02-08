365418e0668dc256012e631d92399b2cb2be8f1e
Aleksey Zamulla <aleksey.zamulla@jetbrains.com>
2024-02-08 13:06:50 +0400

    checksum

--- Package.swift
+++ Package.swift
@@ -1,16 +1,15 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
    name: "shared",
    platforms: [
-     .iOS(.v14)
+     .iOS(.v14),
    ],
    products: [
       .library(name: "shared", targets: ["shared"])
    ],
    targets: [
-      .binaryTarget(name: "shared", url: "https://github.com/zamulla/spmtest/releases/download/2/shared.xcframework.zip")
+            .binaryTarget(name: "shared", url: "https://github.com/zamulla/spmtest/releases/download/2/shared.xcframework.zip",checksum:"575af8a01f09684e8cc35d35a33823cf117f78da31d5bf792cbcae6b8fe861f1")
    ],
-   swiftLanguageVersions: [.v5]
 )
