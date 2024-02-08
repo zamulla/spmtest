33487d2f31efd2cb9ec7310e8dacf49c12e095cc
Aleksey Zamulla <aleksey.zamulla@jetbrains.com>
2024-02-08 13:00:44 +0400

    format?

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
    name: "shared",
    platforms: [
-     .iOS(.v14),
+     .iOS(.v14)
    ],
    products: [
       .library(name: "shared", targets: ["shared"])
