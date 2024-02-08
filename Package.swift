47c0cd2b61612579d4f86555750270fc78844cc1
Aleksey Zamulla <aleksey.zamulla@jetbrains.com>
2024-02-08 13:40:22 +0400

    new release

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
       .library(name: "shared", targets: ["shared"])
    ],
    targets: [
-            .binaryTarget(name: "shared", url: "https://github.com/zamulla/spmtest/releases/download/2/shared.xcframework.zip",checksum:"575af8a01f09684e8cc35d35a33823cf117f78da31d5bf792cbcae6b8fe861f1")
+            .binaryTarget(name: "shared", url: "https://github.com/zamulla/spmtest/releases/download/2/shared.xcframework.zip",checksum:"cf37ba1310758d808e4a276aa00fee1f89173d1cc8645722f1f9ab77c5676937")
    ]
 )
