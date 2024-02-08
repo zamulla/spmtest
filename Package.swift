9f07aa3b1f968439c40b3e0dba7b8f53b691e2ff
Aleksey Zamulla <aleksey.zamulla@jetbrains.com>
2024-02-08 17:21:27 +0400

    ExampleDependency release

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
       .library(name: "shared", targets: ["shared"])
    ],
    targets: [
-            .binaryTarget(name: "shared", url: "https://github.com/zamulla/spmtest/releases/download/4/shared.xcframework.zip",checksum:"7c89c64a8908d9e55c8c0093ad58945c0791ae2ee281ce6264809dedf74f7cc1")
+            .binaryTarget(name: "shared", url: "https://github.com/zamulla/spmtest/releases/download/5/shared.xcframework.zip",checksum:"6ad6e121869bf07e3d4fb6ccc82320c224fc971f123cc53baad997fccd70572f")
    ]
 )
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
     
     jvm()
 
+    val frameworkName = "ExampleDependency"
     val xcf = XCFramework()
 
     listOf(
@@ -24,8 +25,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "Shared"
-            isStatic = true
+            baseName = frameworkName
             xcf.add(this)
         }
     }
