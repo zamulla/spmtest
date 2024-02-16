a84187f8b4fe379247a9ae33fd6e2a75c3e5f7ac
Aleksey Zamulla <aleksey.zamulla@jetbrains.com>
2024-02-16 10:36:37 +0400

    umbrella module Package.swift

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
       .library(name: "ExampleDependency", targets: ["umbrella"])
    ],
    targets: [
-       .binaryTarget(name: "umbrella", url: "https://github.com/zamulla/spmtest/releases/download/6/umbrella.xcframework.zip",checksum:"3c648988f81ecc60d93ff7ef5129869f246a3b5b4d8ae3bb547669363493cd14")
+       .binaryTarget(name: "umbrella", url: "https://github.com/zamulla/spmtest/releases/download/7/umbrella.xcframework.zip",checksum:"3c648988f81ecc60d93ff7ef5129869f246a3b5b4d8ae3bb547669363493cd14")
    ]
 )
